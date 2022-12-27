2a0c50eeca20ef7666dda508f9d80e5cef93dc4e
shuhui <84600899@qq.com>
2022-12-27 17:11:15 +0800

    添加阴影大小的切换选项
    添加阴影大小的切换选项

--- Assets/Scripts/ShadowmapCamera.cs
+++ Assets/Scripts/ShadowmapCamera.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿//https://zhuanlan.zhihu.com/p/454970727
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -6,11 +8,16 @@ using UnityEngine.Rendering;
 [ExecuteInEditMode]
 public class ShadowmapCamera : MonoBehaviour
 {
-
     public enum ShadowType
     {
-        UNITY, PCF, ESM, VSM, EVSM,MY_ESM
+        UNITY,
+        PCF,
+        ESM,
+        VSM,
+        EVSM,
+        MY_ESM
     }
+
     public RenderTexture smTex;
     public RenderTexture esmTex;
     public RenderTexture vsmTex;
@@ -18,83 +25,105 @@ public class ShadowmapCamera : MonoBehaviour
     public bool buildShadowmaps;
     public Camera lightCamera;
     private Shader lightingShader;
+    private int shadowmapSize;
+
     public ShadowType shadowType = ShadowType.UNITY;
-    const int shadowmapSize = 2048;
-    [Range(0, 0.1f)]
-    public float esmNormalBias = 0.001f;
+
+    public enum ShadowmapSizeValue
+    {
+        _256 = 256,
+        _512 = 512,
+        _1024 = 1024,
+        _2048 = 2048,
+        _4096 = 4096
+    }
+
+    public ShadowmapSizeValue shadowmapSizeValue = ShadowmapSizeValue._1024;
+    //const int shadowmapSize = 2048;
+
+    [Range(0, 0.1f)] public float esmNormalBias = 0.001f;
+
     void Start()
     {
-
     }
-    
+
+    private void OnValidate()
+    {
+        shadowmapSize = (int) shadowmapSizeValue;
+        Debug.Log("阴影的大小为：" + shadowmapSize);
+    }
 
     // Update is called once per frame
     void Update()
     {
         var cam = lightCamera;
-        
+
         if (cam == null) return;
         cam.aspect = 1;
-        Shader.SetGlobalMatrix("light_VP", GL.GetGPUProjectionMatrix(cam.projectionMatrix, false) * cam.worldToCameraMatrix);
-        Shader.SetGlobalInt("shadowType", (int)shadowType);
+        Shader.SetGlobalMatrix("light_VP",
+            GL.GetGPUProjectionMatrix(cam.projectionMatrix, false) * cam.worldToCameraMatrix);
+        Shader.SetGlobalInt("shadowType", (int) shadowType);
         Shader.SetGlobalFloat("esmNormalBias", esmNormalBias);
         if (smTex != null) Shader.SetGlobalTexture("smTex", smTex);
         if (esmTex != null) Shader.SetGlobalTexture("esmTex", esmTex);
         if (vsmTex != null) Shader.SetGlobalTexture("vsmTex", vsmTex);
-        if (buildShadowmaps) {
+        if (buildShadowmaps)
+        {
             buildShadowmaps = false;
-           
-            if (smTex != null) {
+
+            if (smTex != null)
+            {
                 RenderTexture.ReleaseTemporary(smTex);
             }
-            smTex = RenderTexture.GetTemporary(shadowmapSize, shadowmapSize, 24, RenderTextureFormat.RFloat, RenderTextureReadWrite.Linear);
-        
+
+            smTex = RenderTexture.GetTemporary(shadowmapSize, shadowmapSize, 24, RenderTextureFormat.RFloat,
+                RenderTextureReadWrite.Linear);
+
             smTex.filterMode = FilterMode.Point;
             cam.targetTexture = smTex;
-        
 
-            cam.RenderWithShader(Shader.Find("ESM/ShadowmapCaster"),"");
+
+            cam.RenderWithShader(Shader.Find("ESM/ShadowmapCaster"), "");
 
             cam.targetTexture = null;
 
             if (esmTex != null)
             {
-                esmTex.Release() ;
+                esmTex.Release();
             }
-            esmTex =new RenderTexture(shadowmapSize, shadowmapSize, 0, RenderTextureFormat.RFloat  , RenderTextureReadWrite.Linear);
+
+            esmTex = new RenderTexture(shadowmapSize, shadowmapSize, 0, RenderTextureFormat.RFloat,
+                RenderTextureReadWrite.Linear);
             esmTex.autoGenerateMips = false;
             esmTex.useMipMap = false;
             esmTex.filterMode = FilterMode.Bilinear;
             var mat = new Material(Shader.Find("ESM/E_ShadowmapCaster"));
-            mat.mainTexture = smTex;// _MainTex;
-            Graphics.Blit(smTex, esmTex,mat,0 );
+            mat.mainTexture = smTex; // _MainTex;
+            Graphics.Blit(smTex, esmTex, mat, 0);
 
 
-        
             if (vsmTex != null)
             {
                 vsmTex.Release();
             }
-            vsmTex = new RenderTexture(shadowmapSize, shadowmapSize, 0, RenderTextureFormat.RGFloat, RenderTextureReadWrite.Linear);
+
+            vsmTex = new RenderTexture(shadowmapSize, shadowmapSize, 0, RenderTextureFormat.RGFloat,
+                RenderTextureReadWrite.Linear);
             vsmTex.autoGenerateMips = false;
             vsmTex.useMipMap = false;
             vsmTex.filterMode = FilterMode.Bilinear;
-            
-            mat.mainTexture = smTex;// _MainTex;
-            Graphics.Blit(smTex, vsmTex, mat,1 );
- 
 
-
+            mat.mainTexture = smTex; // _MainTex;
+            Graphics.Blit(smTex, vsmTex, mat, 1);
         }
 
         if (lightingShader == null)
             lightingShader = Shader.Find("ESM/DeferredShading");
 
-        if (GraphicsSettings.GetCustomShader(BuiltinShaderType.DeferredShading) != lightingShader) {
+        if (GraphicsSettings.GetCustomShader(BuiltinShaderType.DeferredShading) != lightingShader)
+        {
             GraphicsSettings.SetShaderMode(BuiltinShaderType.DeferredShading, BuiltinShaderMode.UseCustom);
             GraphicsSettings.SetCustomShader(BuiltinShaderType.DeferredShading, lightingShader);
         }
-
     }
-    
 }
---
+++ Assets/Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteAlways]
+public class Test : MonoBehaviour
+{
+    public enum ShadowmapSizeV
+    {
+        _256 = 256,
+        _512 = 512,
+        _1024 = 1024,
+        _2048 = 2048,
+        _4096 = 4096
+    }
+    public ShadowmapSizeV shadowmapSize = ShadowmapSizeV._1024;
+
+    private void Awake()
+    {
+        int vol = (int) shadowmapSize;
+        Debug.Log("阴影的大小为：" + vol);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
